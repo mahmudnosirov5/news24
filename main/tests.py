d6dd54b5df96ce3831de4464e3c725fb7a884687
Mahmud <mahmudnosirov77@gmail.com>
2022-04-09 14:22:16 +0500

    Added yml file

--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,32 @@
 from django.tests import TextCase 
 
 class Tests(TextCase):
-	def add_news(self):
-		pass
+	def testA(self):
+		assertEqual(2, 2)
+
+	def testB(self):
+		assertNotEqual(3, 5)
+
+	def testC(self):
+		assertTrue(True)
+
+	def testD(self):
+		assertFalse(False)
+
+	def testE(self):
+		assertIs(True, True)
+
+	def testF(self):
+		assertIsNot(False, True)
+
+	def testG(self):
+		assertIsNone(None)
+
+	def testH(self):
+		assertIsNotNone(None)
+
+	def testI(self):
+		assertIn(2, [3, 2, 5])
+
+	def testJ(self):
+		assertNotIn(9, [0, 10, 13, 6])
