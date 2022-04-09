f4a7e685d71bba0d6103f174f36f418a99f8e0f9
Mahmud <mahmudnosirov77@gmail.com>
2022-04-09 14:46:08 +0500

    YML 1.7

--- main/tests.py
+++ main/tests.py
@@ -2,31 +2,31 @@ from django.test import TestCase
 
 class Tests(TestCase):
 	def testA(self):
-		assertEqual(2, 2)
+		self.assertEqual(2, 2)
 
 	def testB(self):
-		assertNotEqual(3, 5)
+		self.assertNotEqual(3, 5)
 
 	def testC(self):
-		assertTrue(True)
+		self.assertTrue(True)
 
 	def testD(self):
-		assertFalse(False)
+		self.assertFalse(False)
 
 	def testE(self):
-		assertIs(True, True)
+		self.assertIs(True, True)
 
 	def testF(self):
-		assertIsNot(False, True)
+		self.assertIsNot(False, True)
 
 	def testG(self):
-		assertIsNone(None)
+		self.assertIsNone(None)
 
 	def testH(self):
-		assertIsNotNone(None)
+		self.assertIsNotNone(None)
 
 	def testI(self):
-		assertIn(2, [3, 2, 5])
+		self.assertIn(2, [3, 2, 5])
 
 	def testJ(self):
-		assertNotIn(9, [0, 10, 13, 6])
+		self.assertNotIn(9, [0, 10, 13, 6])
